ba62402275f136f4430d6df9d09f6037c8278ff2
jhess37 <jhess37@gatech.edu>
2017-01-29 21:13:07 -0500

    Updated AntLabMain with my name and ID

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -65,8 +65,8 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  toPrint += p2.toString("jpinder3");
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
-		  Person3 p3 = new Person3("P3");
-		  toPrint += p3.toString("gburdell3");
+		  Person3 p3 = new Person3("John Hess");
+		  toPrint += p3.toString("jhess37");
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
                   Person4 p4 = new Person4("P4");
