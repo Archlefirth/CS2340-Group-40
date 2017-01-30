d25f1b538ae46b8c4c192cc75ee1c7c754910801
jhpinder <jhpinder@gmail.com>
2017-01-29 20:31:43 -0500

    Updated Person2

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -11,37 +11,49 @@ package edu.gatech.oad.antlab.person;
 public class Person2 {
     /** Holds the persons real name */
     private String name;
-	 	/**
-	 * The constructor, takes in the persons
-	 * name
-	 * @param pname the person's real name
-	 */
-	 public Person2(String pname) {
-	   name = pname;
-	 }
-	/**
-	 * This method should take the string
-	 * input and return its characters in
-	 * random order.
-	 * given "gtg123b" it should return
-	 * something like "g3tb1g2".
-	 *
-	 * @param input the string to be modified
-	 * @return the modified string
-	 */
-	private String calc(String input) {
-	  //Person 2 put your implementation here
-	  return null;
-	}
-	/**
-	 * Return a string rep of this object
-	 * that varies with an input string
-	 *
-	 * @param input the varying string
-	 * @return the string representing the 
-	 *         object
-	 */
-	public String toString(String input) {
-	  return name + calc(input);
-	}
+    /**
+     * The constructor, takes in the persons
+     * name
+     * @param pname the person's real name
+     */
+    public Person2(String pname) {
+        name = pname;
+    }
+    /**
+     * This method should take the string
+     * input and return its characters in
+     * random order.
+     * given "gtg123b" it should return
+     * something like "g3tb1g2".
+     *
+     * @param input the string to be modified
+     * @return the modified string
+     */
+    private String calc(String input) {
+        java.util.Random r = new java.util.Random();
+        String out = "";
+        int n;
+        java.util.List index = new java.util.ArrayList<Integer>(input.length());
+        while (index.size() < input.length()) {
+            n = r.nextInt(input.length());
+            if (!index.contains(n)) {
+                index.add(n);
+            }
+        }
+        for (int i = 0; i < input.length(); i++) {
+            out = out + input.charAt((int) index.get(i));
+        }
+        return out;
+    }
+    /**
+     * Return a string rep of this object
+     * that varies with an input string
+     *
+     * @param input the varying string
+     * @return the string representing the
+     *         object
+     */
+    public String toString(String input) {
+        return name + calc(input);
+    }
 }
