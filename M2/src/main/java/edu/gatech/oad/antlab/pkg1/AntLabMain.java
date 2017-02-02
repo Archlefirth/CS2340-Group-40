36375ddbf720e9be6591e3983baf6f1d557ba1bf
jhpinder <jhpinder@gmail.com>
2017-02-01 23:44:36 -0500

    updated antlabmain to have names

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -69,12 +69,12 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  toPrint += p3.toString("jhess37");
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
-                  Person4 p4 = new Person4("P4");
-                  toPrint += p4.toString("gburdell4");
+                  Person4 p4 = new Person4("Ishan Waykul");
+                  toPrint += p4.toString("iwaykul3");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("gburdell5");
+                  Person5 p5 = new Person5("Awnish Choudhary");
+                  toPrint += p5.toString("achoudhary34");
 		  
         System.out.println(toPrint);
         
